2f8324760da8f12aaaadcd29da816c6d7990054c
Richard Cho <96richardohc@gmail.com>
2020-05-18 22:59:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog 
 
-def name=(name)
-  @name = name
-end
+  def name=(name)
+    @name = name
+  end
 
-def name 
-  @name
-end
+  def name 
+    @name
+  end
 
-def breed=(breed_name)
-  @breed = (breed_name)
-end
+  def breed=(breed_name)
+    @breed = (breed_name)
+  end
 
-def breed
-  @breed
-end 
+  def breed
+    @breed
+  end 
 end
